956866b3349dca4c4a9a5794a79ce7c9a6c51a89
cpzzansu <138553212+cpzzansu@users.noreply.github.com>
2023-09-12 16:26:19 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 140 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+
+        long sum = 0;
+
+        for (int i = 1; i <= n - 2; i++) {
+            sum += ((n - (i + 1)) * ((n - (i + 1)) + 1)) / 2;
+        }
+
+        System.out.println(sum);
+        System.out.println("3");
+    }
+}
