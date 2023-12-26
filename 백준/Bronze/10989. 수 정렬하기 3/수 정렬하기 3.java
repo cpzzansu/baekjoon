7ecd3a4e5e45cb93e3b0d44c3fac297b6a109032
cpzzansu <138553212+cpzzansu@users.noreply.github.com>
2023-12-26 15:06:12 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2784 ms, Memory: 342116 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] notYetSort = new int[n];
+
+        for(int i = 0; i < n; i++){
+            notYetSort[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(notYetSort);
+
+        for(int sort : notYetSort){
+            bw.write(String.valueOf(sort));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
